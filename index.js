c5da234913f5db19cf144eb43f2554eceb4120c7
najma099 <najmakhatun0999@gmail.com>
2025-05-15 14:45:02 +0530

    backend

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+console.log(process.env.PORT);
+let port = process.env.PORT || 5120;
+
+app.listen(port, () => {
+  console.log(`Backend service is running on : http://localhost:${port}/`);
+});
+
+app.get("/test", (req, res) => {
+  console.log(req.params);
+  res.send("Test endpoint reached");
+});
